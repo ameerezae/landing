4bf04392b43608d96554be3dd5599e8a47667ed8
ameerezae <amir.rezaei1999@yahoo.com>
2025-02-28 17:31:05 +0330

    Restructure files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 'use client';
-import Button from '@/components/basic/button/button';
-import Image from 'next/image';
-import download from '@/icons/download.svg';
 
 export default function Home() {
   return <div></div>;
